277836a02076bd45bc3f645dd715e77e3c420e0a
Joffry Ferrater <joffry.ferrater@axiomatics.com>
2017-06-08 12:34:29 +0200

    first commit. project setup

---
+++ com.github.joffryferrater/src/main/java/com/github/joffryferrater/Main.java
@@ -0,0 +1,11 @@
+package com.github.joffryferrater;
+
+public class Main {
+
+	public static void main(String[] args) {
+		String path = System.getProperty("log4j.configuration");
+		
+		System.out.println("Path config : " + path);
+	}
+
+}
